97b22eb6ef8d2c017e14cadf92e30de2ad49e3e2
reyhane-code <reyhane.code@gmail.com>
2024-04-12 22:36:54 +0330

    fix(sortselector): fix typo

--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -21,7 +21,6 @@ const SortSelector = () => {
   return (
     <details className="dropdown">
       <summary className="m-1 btn">
-        {" "}
         Order by: {currentSortOrder?.label || "Relevance"}
       </summary>
       <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
