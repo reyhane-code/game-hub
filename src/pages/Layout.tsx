61e52ca6d08952df482b002daa9cd880181739f9
reyhane-code <reyhane.code@gmail.com>
2024-07-08 19:04:14 +0330

    delete footer

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,4 +1,3 @@
-import Footer from "../components/Footer";
 import NavBar from "../components/NavBar";
 import { Outlet } from "react-router-dom";
 
@@ -9,7 +8,6 @@ const Layout = () => {
       <div className="p-5">
         <Outlet />
       </div>
-      <Footer />
     </>
   );
 };
