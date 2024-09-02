96584d613c5cec6fb836ea83a496cbe90d85a3eb
mortezahatamikia-code <mortezahatamikia.code@gmail.com>
2024-09-02 13:39:50 +0330

    fix(useAuth): setUserIdentity check null response

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -30,8 +30,10 @@ export default function useAuth() {
       const response = await HttpRequest.get<User>("/v1/user/identity", {
         signal,
       });
-      setIdentity(response.data);
-      return response.data;
+      if (response?.data) {
+        setIdentity(response.data);
+        return response.data;
+      }
     } catch (error) {
       console.error("Error fetching user data:", error);
       throw new Error("Failed to fetch user data");
